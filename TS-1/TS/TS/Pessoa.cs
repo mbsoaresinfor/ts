26c47bbb41d1646aaddd862effb6f080f7cb466a
Marcelo Soares <marcelo@rodosoft.com.br>
2023-04-18 14:47:10 -0300

    aula 1

---
+++ TS-1/TS/TS/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS
+{
+    public class Pessoa
+    {
+
+        public String concatenaNomeSobreNome(String nome, String sobreNome)
+        {
+            return nome + sobreNome ;
+        }
+
+        public Boolean eMaiorIdade(int idade)
+        {
+            if(idade >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+    }
+}
---
+++ TS-1/TS/TS_TEST/PessoaTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS;
+
+namespace TS_TEST
+{
+    class PessoaTest
+    {
+
+        [Test]
+        public void testConcatenaNomeSobreNome()
+        {
+            String nome = "Pedro";
+            String sobreNome = "Silva";
+            Pessoa pessoa = new Pessoa();
+            String resultadoPrograma = pessoa.concatenaNomeSobreNome(nome, sobreNome);
+            Assert.AreEqual("PedroSilva", resultadoPrograma);
+        }
+
+        [Test]
+        public void testEMaiorIdade() {
+            int entrada = 18;
+            Pessoa pessoa = new Pessoa();
+            Boolean resutladoPrograma = pessoa.eMaiorIdade(entrada);
+            Assert.AreEqual(true, resutladoPrograma);
+        }
+
+        [Test]
+        public void testEMaiorIdadeComValorAbaixo18()
+        {
+            int entrada = 17;
+            Pessoa pessoa = new Pessoa();
+            Boolean resutladoPrograma = pessoa.eMaiorIdade(entrada);
+            Assert.AreEqual(false, resutladoPrograma);
+        }
+
+
+    }
+}
