ecf517d686cb50ab5e1bfd548e8293172d5e32dd
Marcelo Soares <marcelo@rodosoft.com.br>
2023-04-19 14:59:19 -0300

    aula 2

---
+++ TS-2/TS/TS/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS
+{
+    public class Pessoa
+    {
+        public String concatenaNomeSobreNome(String nome, String sobreNome)
+        {
+            return nome + sobreNome ;
+        }
+
+    }
+}
---
+++ TS-2/TS/TS_TEST/PessoaTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS;
+
+namespace TS_TEST
+{
+    class PessoaTest
+    {
+        [Test]
+        public void testConcatenaNomeSobreNome()
+        {
+            String nome = "Pedro";
+            String sobreNome = "Silva";
+            Pessoa pessoa = new Pessoa();
+            String resultadoPrograma = pessoa.concatenaNomeSobreNome(nome, sobreNome);
+            Assert.AreEqual("PedroSilva", resultadoPrograma);
+        }
+
+
+    }
+}
