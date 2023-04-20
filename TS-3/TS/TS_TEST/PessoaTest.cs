05f8bf815b389dec5bfc938a32545e5fa29b673b
Marcelo Soares <marcelo@rodosoft.com.br>
2023-04-20 14:44:25 -0300

    aula 2

---
+++ TS-3/TS/TS_TEST/PessoaTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS;
+
+namespace TS_TEST
+{
+    class PessoaTest
+    {
+        [Test]
+        public void testConcatenaNomeSobreNome()
+        {
+            String nome = "Pedro";
+            String sobreNome = "Silva";
+            Pessoa pessoa = new Pessoa();
+            String resultadoPrograma = pessoa.concatenaNomeSobreNome(nome, sobreNome);
+            Assert.AreEqual("PedroSilva", resultadoPrograma);
+        }
+
+        [Test]
+        public void testConcatenaNomeSobreNomeVazio()
+        {
+            String nome = String.Empty;
+            String sobreNome = String.Empty;
+            Pessoa pessoa = new Pessoa();
+            String resultadoPrograma = pessoa.concatenaNomeSobreNome(nome, sobreNome);
+            Assert.AreEqual(String.Empty, resultadoPrograma);
+        }
+
+        [Test]
+        public void testConcatenaNomeSobreNomeComNull()
+        {
+            String nome = null;
+            String sobreNome = null;
+            Pessoa pessoa = new Pessoa();
+            String resultadoPrograma = pessoa.concatenaNomeSobreNome(nome, sobreNome);
+            Assert.AreEqual(String.Empty, resultadoPrograma);
+        }
+
+    }
+}
