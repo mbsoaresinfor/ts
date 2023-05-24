4517041776e6f5161a5404ccce41c8c2e00e607a
Marcelo Soares <marcelo@rodosoft.com.br>
2023-05-24 13:56:47 -0300

    aula 7

---
+++ TS-2/TS/TS/ValidaCarro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS
+{
+    public class ValidaCarro
+    {
+        public bool validaCarro(Carro carro) 
+        {
+            
+       
+        if(carro.codigo < 0 ){
+          return false;
+        }
+
+        if(carro.modelo.Length > 20) { 
+            return false;
+        }
+        
+        return true;
+        
+    }
+}
+}
---
+++ TS-2/TS/TS_TEST/ValidaCarroTest.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS;
+
+namespace TS_TEST
+{
+    public class ValidaCarroTest
+    {
+
+        [Test]
+        public void testIdCArroOk()
+        {
+            // arrange
+            ValidaCarro validaCarro = new ValidaCarro();
+            Carro carro = criaCarro();
+            carro.codigo = 1;
+            bool resultadoExperado = true;
+            bool resultadoPrograma;
+
+            // act
+            resultadoPrograma = validaCarro.validaCarro(carro);
+
+            // assert
+            Assert.AreEqual(resultadoExperado, resultadoPrograma);
+
+
+        }
+
+        [Test]
+        public void testIdCArroError()
+        {
+            // arrange
+            ValidaCarro validaCarro = new ValidaCarro();
+            Carro carro = criaCarro();
+            carro.codigo = -1;
+            bool resultadoExperado = false;
+            bool resultadoPrograma;
+
+            // act
+            resultadoPrograma = validaCarro.validaCarro(carro);
+
+            // assert
+            Assert.AreEqual(resultadoExperado, resultadoPrograma);
+
+
+        }
+
+        private Carro criaCarro()
+        {
+            Carro carro = new Carro();
+            carro.codigo = 1;
+            carro.modelo = "gol";
+            return carro;
+        }
+    }
+}
